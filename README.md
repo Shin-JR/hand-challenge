e40fa5df8761abcd214ea9676b0de21d438ff59e
Shin-JR <j.riquelme@uc.cl>
2023-10-02 01:55:33 -0300

    Finished

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# hand-challenge
-This repository contains the solution to a challenge in this repo: https://github.com/jesus-seijas-sp/hand-challenge
+# package.json
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.js
+```
+
+This project was created using `bun init` in bun v1.0.2. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ index_test.js
@@ -0,0 +1,15 @@
+import { expect, test } from "bun:test";
+import translate from "./index.js";
+
+
+test("Hello translated correctly", () => {
+  expect(
+    translate("👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊")
+    ).toBe("Hello");
+});
+
+test("Hello World! translated correctly", () => {
+  expect(
+    translate("👉👆👆👆👆👆👆👆👆🤜👇👈👆👆👆👆👆👆👆👆👆👉🤛👈👊👉👉👆👉👇🤜👆🤛👆👆👉👆👆👉👆👆👆🤜👉🤜👇👉👆👆👆👈👈👆👆👆👉🤛👈👈🤛👉👇👇👇👇👇👊👉👇👉👆👆👆👊👊👆👆👆👊👉👇👊👈👈👆🤜👉🤜👆👉👆🤛👉👉🤛👈👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👉👊👆👆👆👊👇👇👇👇👇👇👊👇👇👇👇👇👇👇👇👊👉👆👊👉👆👊")
+    ).toBe("Hello World!\n");
+  });
